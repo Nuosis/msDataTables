e7a11b1d0bd8d1dd8748a9b0d5652a3f6710e1c4
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-04-28 11:49:44 -0700

    districtChurch

--- src/index.js
+++ src/index.js
@@ -7,30 +7,91 @@ let table;
 
 window.loadTable = (json) => {
   const obj = JSON.parse(json);
-  console.log(obj);
   const data = obj.data;
-  const columns = [
-    { data: "fieldData.CompanyName", title: "Name" },
-    { data: "fieldData.City", title: "City" },
-    { data: "fieldData.State", title: "State" },
-    { data: "fieldData.Zip", title: "Zip Code", orderable: false },
-  ];
+  // console.log(data);
+  const columns = obj.columns;
+  // console.log(columns);
   table = $("#dtable").DataTable({
-    columns,
+    destroy: true,
     data,
+    columns,
+    columnDefs: [
+      {
+        targets: 6,
+        title: "Note",
+        data: null,
+        orderable: false,
+        defaultContent: "<button id='note' class='btn btn-outline-secondary'><span class='btn-label'><i class='journal-text'></i></span>+</button>",
+      },
+      {
+        targets: 7,
+        data: null,
+        orderable: false,
+        defaultContent: "<button id='delete' class='btn btn-outline-danger'><span class='btn-label'><i class='trash3'></i></span>X</button>",
+      },
+      {
+        target: 0,
+        visible: false,
+        searchable: false,
+        // set column to dollar format
+        // render: DataTable.render.number(null, null, 2, '$'),
+      },
+      {
+        target: 1,
+        visible: false,
+        searchable: false,
+      },
+      {
+        target: 2,
+        type: Date,
+      }
+    ],
+
+    // BUTTONS
+    /* dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'excelHtml5',
+        title: 'Customer Sales',
+        filename: 'Customer Sales ' + new Date(),
+        extension: '.xlsx',
+      },
+      {
+        extend: 'pdfHtml5',
+        title: 'Customer Sales',
+        download: 'download',
+        orientation: 'portrait',
+        pagesize: 'LETTER',
+        filename: 'Customer Sales ' + new Date(),
+        extension: '.pdf',
+      }
+    ], */
     colReorder: true,
     responsive: true,
-    paging: true,
+    paging: false,
+    pageLength: 25,
     ordering: true,
+    order: [
+      [2, "desc"],
+    ],
   });
 };
-
+/*
 $("#dtable").on("click", "tr", function () {
   const row = table.row(this);
   const data = row.data();
-  const fmID = data.fieldData.PrimaryKey;
-  const recordID = data.recordId;
-  const obj = { ID: fmID, recordID: recordID };
+  const obj = { record: data };
+  FileMaker.PerformScript("Report . Invoice . customerDetails", JSON.stringify(obj));
+}) */
 
-  FileMaker.PerformScript("showRowDetails", JSON.stringify(obj));
-})
+$("#dtable").on("click", "#delete", function () {
+  const data = table.row($(this).parents('tr')).data();
+  const obj = { record: data, button: "delete" };
+  FileMaker.PerformScript("dataTable . showRowDetails", JSON.stringify(obj))
+});
+
+$("#dtable").on("click", "#note", function () {
+  const data = table.row($(this).parents('tr')).data();
+  const obj = { record: data, button: "note" };
+  FileMaker.PerformScript("dataTable . showRowDetails", JSON.stringify(obj))
+});
